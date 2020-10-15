dc91d1641da704ddf32dc8654f0fcae330c1995b
andrew2089 <aglushenok9@gmail.com>
2020-10-15 11:09:24 +0300

    updatted hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world bastard");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Hello world bastard");
+        }
+
     }
 
 }
